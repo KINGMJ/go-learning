719090b7877c9f8bf834578af786e1ed1699eea6
KINGMJ <328047478@qq.com>
2023-08-07 16:29:41 +0800

    读写锁

--- demo21/rwmutex/main.go
+++ demo21/rwmutex/main.go
@@ -14,6 +14,10 @@ var (
 )
 
 func main() {
+	demo2()
+}
+
+func demo1() {
 	start := time.Now()
 	for i := 0; i < 10; i++ {
 		wg.Add(1)
@@ -43,4 +47,43 @@ func read() {
 	time.Sleep(10 * time.Millisecond)
 	rwlock.RUnlock()
 	defer wg.Done()
+}
+
+// ----------- (●'◡'●)(●'◡'●)(●'◡'●)(●'◡'●)(●'◡'●)(●'◡'●)(●'◡'●)(●'◡'●)(●'◡'●)(●'◡'●) ------------
+
+// 一个线程安全的计数器
+type Counter struct {
+	mu    sync.RWMutex
+	count uint64
+}
+
+func (c *Counter) Incr() {
+	c.mu.Lock()
+	c.count++
+	c.mu.Unlock()
+}
+
+func (c *Counter) Count() uint64 {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+	return c.count
+}
+
+func demo2() {
+	var counter Counter
+	// 10个线程去读取
+	for i := 0; i < 10; i++ {
+		go func() {
+			for {
+				count := counter.Count() // 计数器读操作
+				fmt.Println(count)
+				time.Sleep(time.Millisecond)
+			}
+		}()
+	}
+	// 每隔5s写一次
+	for {
+		counter.Incr() // 计数器写操作
+		time.Sleep(5 * time.Second)
+	}
 }
--- demo21/trylock/main.go
+++ demo21/trylock/main.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"fmt"
+	"math/rand"
 	"sync"
 	"sync/atomic"
+	"time"
 	"unsafe"
 )
+
+func main() {
+	demo1()
+}
 
 const (
 	mutexLocked      = 1 << iota // 加锁标志位置
@@ -19,7 +26,77 @@ type Mutex struct {
 }
 
 func (m *Mutex) TryLock() bool {
+	// 如果能成功抢到锁
 	if atomic.CompareAndSwapInt32((*int32)(unsafe.Pointer(&m.Mutex)), 0, mutexLocked) {
 		return true
 	}
+	// 如果处于唤醒、加锁或者饥饿状态，这次请求就不参与竞争了，返回false
+	old := atomic.LoadInt32((*int32)(unsafe.Pointer(&m.Mutex)))
+
+	if old&(mutexLocked|mutexStarving|mutexWoken) != 0 {
+		return false
+	}
+	// 尝试在竞争的状态下请求锁
+	new := old | mutexLocked
+	return atomic.CompareAndSwapInt32((*int32)(unsafe.Pointer(&m.Mutex)), old, new)
+}
+
+func (m *Mutex) Count() int {
+	// 获取 state 字段的值
+	v := atomic.LoadInt32((*int32)(unsafe.Pointer(&m.Mutex)))
+	v = v >> mutexWaiterShift //得到等待者的数值
+	v = v + (v & mutexLocked)
+	return int(v)
+}
+
+// 锁是否被持有
+func (m *Mutex) IsLocked() bool {
+	state := atomic.LoadInt32((*int32)(unsafe.Pointer(&m.Mutex)))
+	return state&mutexLocked == mutexLocked
+}
+
+// 是否有等待者被唤醒
+func (m *Mutex) IsWoken() bool {
+	state := atomic.LoadInt32((*int32)(unsafe.Pointer(&m.Mutex)))
+	return state&mutexWoken == mutexWoken
+}
+
+// 锁是否处于饥饿状态
+func (m *Mutex) IsStarving() bool {
+	state := atomic.LoadInt32((*int32)(unsafe.Pointer(&m.Mutex)))
+	return state&mutexStarving == mutexStarving
+}
+
+func demo1() {
+	var mu Mutex
+	go func() {
+		mu.Lock()
+		// 休眠0~5s
+		second := time.Duration(rand.Intn(6)) * time.Second
+		time.Sleep(second)
+		mu.Unlock()
+	}()
+
+	time.Sleep(time.Second)
+	ok := mu.TryLock() // 尝试获取到锁
+	if ok {
+		fmt.Println("got the lock")
+		mu.Unlock()
+		return
+	}
+	fmt.Println("can't get the lock")
+}
+
+func demo2() {
+	var mu Mutex
+	for i := 0; i < 1000; i++ {
+		go func() {
+			mu.Lock()
+			time.Sleep(time.Second)
+			mu.Unlock()
+		}()
+	}
+	time.Sleep(time.Second)
+	// 输出锁的信息
+	fmt.Printf("waitings: %d, isLocked: %t, woken: %t, starving: %t\n", mu.Count(), mu.IsLocked(), mu.IsWoken(), mu.IsStarving())
 }
