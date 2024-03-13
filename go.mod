47d85a7965758ea95626bedd635f14c7f709398e
KINGMJ <328047478@qq.com>
2024-03-13 16:14:54 +0800

    update demo

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/KINGMJ/go-learning
+
+go 1.21.0
--- main.go
+++ main.go
@@ -3,25 +3,22 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"strings"
+	"os"
 )
 
 func main() {
-	// 创建一个包含换行符的字符串读取器
-	reader := strings.NewReader("Apple\nOrange\nBanana")
-
-	// 创建一个 Scanner，并使用默认的 Split 方法（按行分割）
-	scanner := bufio.NewScanner(reader)
-
-	// 使用 Scan 方法逐行扫描数据
+	counts := make(map[string]int)
+	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
-		// 获取扫描到的数据
 		line := scanner.Text()
-		// 对数据进行处理，这里简单打印
-		fmt.Println(line)
+		if line == "end" {
+			break
+		}
+		counts[line]++
 	}
-	// 检查是否有扫描过程中的错误
-	if err := scanner.Err(); err != nil {
-		fmt.Println("Error:", err)
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
 	}
 }
